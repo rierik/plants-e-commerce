95557ee3db8ee2c657da80407bb286173e6dc67d
khr <kwonhr@easywith.com>
2025-04-03 15:10:19 +0900

    Feat: 메인화면 가이드라인 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,187 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="max-w-custom font-[family-name:var(--font-geist-sans)] w-">
+      <header className="grid grid-cols-[160px_auto_120px] items-center w-full h-16 p-1.5  ">
+        <a className="w-40 h-10" href="">
+          <Image src="/logo/logohorizontal.png" alt="로고이미지" width={160} height={40}></Image>
+        </a>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <ul className="flex gap-8 items-center justify-center">
+          <li>
+            <a href="">HOME</a>
+          </li>
+          <li>
+            <a href="">베스트</a>
+          </li>
+          <li>
+            <a href="">신상품</a>
+          </li>
+          <li>
+            <a href="">카테고리</a>
+          </li>
+        </ul>
+
+        <ul className="flex items-center gap-4 justify-end">
+          <li>
+            <a className="inline-block w-8 h-8  rounded-full bg-black" href=""></a>
+          </li>
+          <li>
+            <a href="">장바구니</a>
+          </li>
+        </ul>
+      </header>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="w-full h-140 bg-blue-200">배너</div>
+        <div className="w-full">
+          <section className="my-10">
+            <h2 className="flex items-center justify-between py-4">
+              <p className="title-p">베스트</p>
+              <span>더보기</span>
+            </h2>
+            <ul className="w-full grid grid-cols-[repeat(4,1fr)] gap-10">
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section className="my-10">
+            <h2 className="flex items-center justify-between py-4">
+              <p className="title-p">신상품</p>
+              <span>더보기</span>
+            </h2>
+            <ul className="w-full grid grid-cols-[repeat(4,1fr)] gap-10">
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section className="my-10">
+            <h2 className="flex items-center justify-between py-4">
+              <p className="title-p">시즌제품</p>
+              <span>더보기</span>
+            </h2>
+            <ul className="w-full grid grid-cols-[repeat(4,1fr)] gap-10">
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a className="bg-amber-100 inline-block w-full" href="">
+                  <Image className="w-full h-auto" width={269} height={269} src={'/favicon.ico'} alt=""></Image>
+                  <div>
+                    <span>필터링</span>
+                    <h3>상품명</h3>
+                    <p>가격</p>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </section>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
