6987f6ad64ac7d2151ebc48539ea6db794385285
khr <kwonhr@easywith.com>
2025-04-10 11:50:25 +0900

    Feat: 배너 슬라이드 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,36 @@
+'use client';
+
 import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Scrollbar, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export default function Home() {
+  const slideData = [
+    {
+      id: 1,
+      text: '테스트 테스트',
+    },
+    {
+      id: 2,
+      text: '테스트 테스트',
+    },
+    {
+      id: 3,
+      text: '테스트 테스트',
+    },
+    {
+      id: 4,
+      text: '테스트 테스트',
+    },
+    {
+      id: 5,
+      text: '테스트 테스트',
+    },
+  ];
+
   return (
     <div className="max-w-custom font-[family-name:var(--font-geist-sans)] w-">
       <header className="grid grid-cols-[160px_auto_120px] items-center w-full h-16 p-1.5  ">
@@ -33,7 +63,28 @@ export default function Home() {
         </ul>
       </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="w-full h-140 bg-blue-200">배너</div>
+        <div className="w-full h-140 bg-blue-200">
+          <div className="h-full swiper-container">
+            <Swiper
+              modules={[Navigation, Scrollbar, Autoplay]} // 이렇게 넘기는 방식이 최신
+              loop={true} // 슬라이드 루프
+              navigation={true} // prev, next button
+              autoplay={{
+                delay: 4000,
+                disableOnInteraction: false, // 사용자 상호작용시 슬라이더 일시 정지 비활성
+              }}
+              className="h-full leading-140 text-center"
+            >
+              {slideData.map((slide) => (
+                <SwiperSlide className="w-full" key={slide.id}>
+                  <div>
+                    <div>{slide.text}</div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
         <div className="w-full">
           <section className="my-10">
             <h2 className="flex items-center justify-between py-4">
