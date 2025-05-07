75fd11c8cf2e9aff4ae5b2d8a5fca46c97672339
khr <kwonhr@easywith.com>
2025-05-07 14:06:10 +0900

    feat: 카테고리 서브 헤더로 나올 수 있도록 설정

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -7,7 +7,18 @@ const navItems = [
   { name: 'HOME', path: '/' },
   { name: '베스트', path: '/best' },
   { name: '신상품', path: '/new' },
-  { name: '카테고리', path: '/category' },
+  {
+    name: '카테고리',
+    path: '/category',
+    sub: [
+      { name: '다육이', path: '/category/succulent' },
+      { name: '관엽식물', path: '/category/foliage' },
+      { name: '수경재배식물', path: '/category/hydroponic' },
+      { name: '공기정화식물', path: '/category/air_purifying' },
+      { name: '행운상징 식물', path: '/category/lucky' },
+      { name: '기타', path: '/category/etc' },
+    ],
+  },
 ];
 
 const Header = () => {
@@ -21,7 +32,7 @@ const Header = () => {
   }, [pathname]);
 
   return (
-    <header className="grid grid-cols-[160px_auto_120px] items-center w-full h-16 p-1.5  ">
+    <header className="grid grid-cols-[160px_auto_120px] items-center w-full h-16 ">
       <Link
         href={{
           pathname: '/',
@@ -30,15 +41,27 @@ const Header = () => {
         <Image src="/logo/logohorizontal.png" alt="로고이미지" width={160} height={40}></Image>
       </Link>
 
-      <ul className="flex gap-8 items-center justify-center">
+      <ul className="flex gap-8 items-center justify-center w-full h-[full-1.5] leading-16">
         {navItems.map((item) => {
           const isActive = item.path === '/' ? pathname === '/' : pathname === item.path || pathname.startsWith(`${item.path}/`);
 
           return (
-            <li key={item.path}>
-              <Link href={item.path}>
-                <p className={isActive ? 'text-green-600 font-bold' : ''}>{item.name}</p>
-              </Link>
+            <li key={item.path} className="relative group h-full ">
+              <div className="cursor-pointer">
+                <Link href={item.path}>
+                  <p className={isActive ? 'text-green-600 font-bold' : ''}>{item.name}</p>
+                </Link>
+              </div>
+
+              {item.sub && (
+                <ul className="absolute top-14 left-0 mt-2 w-48 bg-white border rounded shadow-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-200 z-10">
+                  {item.sub.map((subItem) => (
+                    <li key={subItem.path} className="px-4 py-2 hover:bg-gray-100">
+                      <Link href={subItem.path}>{subItem.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           );
         })}
